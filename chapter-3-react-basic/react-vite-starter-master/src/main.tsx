4dcecf85c4f54b4f1f5741b4b30830b9f4e0388e
tringuyen1 <46176254+tringuyen1@users.noreply.github.com>
2023-10-21 19:37:10 +0700

    2

--- chapter-3-react-basic/react-vite-starter-master/src/main.tsx
+++ chapter-3-react-basic/react-vite-starter-master/src/main.tsx
@@ -1,10 +1,26 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.tsx'
-import './index.css'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App.tsx";
+import "./index.css";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
 
-ReactDOM.createRoot(document.getElementById('root')!).render(
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <App />,
+  },
+  {
+    path: "/users",
+    element: <div> manage user</div>,
+  },
+  {
+    path: "/tracks",
+    element: <div> manage tracks</div>,
+  },
+]);
+
+ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+    <RouterProvider router={router} />
+  </React.StrictMode>
+);
