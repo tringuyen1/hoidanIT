afe22786b06acefc66c765d83242792a13a17e0f
tringuyen1 <46176254+tringuyen1@users.noreply.github.com>
2023-10-23 22:06:30 +0700

    chapter-12

--- chapter-3-react-basic/react-vite-starter-master/src/App.tsx
+++ chapter-3-react-basic/react-vite-starter-master/src/App.tsx
@@ -1,10 +1,20 @@
 // import './App.css'
 import InputTodo from "./components/input.todo";
 
+const TestComponent = () => {
+  return (
+    <>
+      <span>text children truyền từ thằng cha xuống thằng con</span>
+    </>
+  );
+};
+
 function App() {
   return (
     <>
-      <InputTodo age={15} />
+      <InputTodo age={15}>
+        <TestComponent />
+      </InputTodo>
     </>
   );
 }
--- chapter-3-react-basic/react-vite-starter-master/src/components/input.todo.tsx
+++ chapter-3-react-basic/react-vite-starter-master/src/components/input.todo.tsx
@@ -1,19 +1,23 @@
+import React from "react";
+
 export interface IProps {
-     name?: string;
-     age?: number;
-     info?: {
-          gender?: string;
-          address?: string;
-     };
+  name?: string;
+  age?: number;
+  info?: {
+    gender?: string;
+    address?: string;
+  };
+  children: React.ReactNode;
 }
 
 const InputTodo = (props: IProps) => {
-     const { age } = props;
-     return (
-          <>
-               <div className="container mt-5">{age}</div>
-          </>
-     );
+  const { age } = props;
+  return (
+    <>
+      <div className="container mt-5">{age}</div>
+      {props.children}
+    </>
+  );
 };
 
 export default InputTodo;
