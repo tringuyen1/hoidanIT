2cb5178d9a4f9933f48549f8c9e634bf46a62183
tringuyen1 <46176254+tringuyen1@users.noreply.github.com>
2023-10-23 23:32:55 +0700

    chapter-12 header

--- chapter-11/nextjs-mui-ts-starter-master/src/app/page.tsx
+++ chapter-11/nextjs-mui-ts-starter-master/src/app/page.tsx
@@ -1,3 +1,11 @@
-export default function HomePage() {
-  return <div>hello world!</div>;
-}
+import AppHeader from "./header/app.header";
+
+const HomePage = () => {
+  return (
+    <div>
+      <AppHeader />
+    </div>
+  );
+};
+
+export default HomePage;
