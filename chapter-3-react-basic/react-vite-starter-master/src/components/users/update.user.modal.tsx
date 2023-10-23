36e6e8096d0efb7f970022eeceaf95166c4f8b5f
tringuyen1 <tri.nguyen@srceen1.io>
2023-10-23 09:21:46 +0700

    2

--- chapter-3-react-basic/react-vite-starter-master/src/components/users/update.user.modal.tsx
+++ chapter-3-react-basic/react-vite-starter-master/src/components/users/update.user.modal.tsx
@@ -3,106 +3,106 @@ import { Modal, Input } from "antd";
 import { IUsers } from "./users.table";
 
 interface IProps {
-  addNewUser: any;
-  isUpdateModalOpen: boolean;
-  setIsUpdateModalOpen: (v: boolean) => void;
-  dataUpdate: null | IUsers;
-  setDataUpdate: any;
-  updateUser: any;
+	addNewUser: any;
+	isUpdateModalOpen: boolean;
+	setIsUpdateModalOpen: (v: boolean) => void;
+	dataUpdate: null | IUsers;
+	setDataUpdate: any;
+	updateUser: any;
 }
 
 const UpdateUserModal = (props: IProps) => {
-  const {
-    isUpdateModalOpen,
-    setIsUpdateModalOpen,
-    dataUpdate,
-    setDataUpdate,
-    updateUser,
-  } = props;
+	const {
+		isUpdateModalOpen,
+		setIsUpdateModalOpen,
+		dataUpdate,
+		setDataUpdate,
+		updateUser,
+	} = props;
 
-  const [name, setName] = useState("");
-  const [email, setEmail] = useState("");
-  const [password, setPassword] = useState("");
-  const [age, setAge] = useState("");
-  const [gender, setGender] = useState("");
-  const [address, setAddress] = useState("");
-  const [role, setRole] = useState("");
+	const [name, setName] = useState("");
+	const [email, setEmail] = useState("");
+	const [password, setPassword] = useState("");
+	const [age, setAge] = useState("");
+	const [gender, setGender] = useState("");
+	const [address, setAddress] = useState("");
+	const [role, setRole] = useState("");
 
-  useEffect(() => {
-    if (isUpdateModalOpen && dataUpdate) {
-      setName(dataUpdate.name);
-      setEmail(dataUpdate.email);
-      //  setPassword(dataUpdate.password);
-      setAge(dataUpdate.age);
-      setGender(dataUpdate.gender);
-      setAddress(dataUpdate.address);
-      setRole(dataUpdate.role);
-    }
-  }, [isUpdateModalOpen, dataUpdate]);
+	useEffect(() => {
+		if (isUpdateModalOpen && dataUpdate) {
+			setName(dataUpdate.name);
+			setEmail(dataUpdate.email);
+			//  setPassword(dataUpdate.password);
+			setAge(dataUpdate.age);
+			setGender(dataUpdate.gender);
+			setAddress(dataUpdate.address);
+			setRole(dataUpdate.role);
+		}
+	}, [isUpdateModalOpen, dataUpdate]);
 
-  const handleOk = () => {
-    const data = {
-      _id: dataUpdate?._id,
-      name,
-      email,
-      password,
-      age,
-      gender,
-      role,
-      address,
-    };
-    updateUser(data);
-    setIsUpdateModalOpen(false);
-    handleCloseCreateModal();
-  };
+	const handleOk = () => {
+		const data = {
+			_id: dataUpdate?._id,
+			name,
+			email,
+			password,
+			age,
+			gender,
+			role,
+			address,
+		};
+		updateUser(data);
+		setIsUpdateModalOpen(false);
+		handleCloseCreateModal();
+	};
 
-  const handleCloseCreateModal = () => {
-    setIsUpdateModalOpen(false);
-    setDataUpdate(null);
-  };
+	const handleCloseCreateModal = () => {
+		setIsUpdateModalOpen(false);
+		setDataUpdate(null);
+	};
 
-  return (
-    <Modal
-      title="Basic Modal"
-      open={isUpdateModalOpen}
-      onOk={handleOk}
-      onCancel={() => handleCloseCreateModal()}
-      maskClosable={false}
-    >
-      <div>
-        <label>Name</label>
-        <Input value={name} onChange={(e) => setName(e.target.value)} />
-      </div>
-      <div>
-        <label>Email</label>
-        <Input value={email} onChange={(e) => setEmail(e.target.value)} />
-      </div>
-      <div>
-        <label>Password</label>
-        <Input
-          value={password}
-          onChange={(e) => setPassword(e.target.value)}
-          disabled
-        />
-      </div>
-      <div>
-        <label>Age</label>
-        <Input value={age} onChange={(e) => setAge(e.target.value)} />
-      </div>
-      <div>
-        <label>Gender</label>
-        <Input value={gender} onChange={(e) => setGender(e.target.value)} />
-      </div>
-      <div>
-        <label>Address</label>
-        <Input value={address} onChange={(e) => setAddress(e.target.value)} />
-      </div>
-      <div>
-        <label>Role</label>
-        <Input value={role} onChange={(e) => setRole(e.target.value)} />
-      </div>
-    </Modal>
-  );
+	return (
+		<Modal
+			title="Basic Modal"
+			open={isUpdateModalOpen}
+			onOk={handleOk}
+			onCancel={() => handleCloseCreateModal()}
+			maskClosable={false}
+		>
+			<div>
+				<label>Name</label>
+				<Input value={name} onChange={(e) => setName(e.target.value)} />
+			</div>
+			<div>
+				<label>Email</label>
+				<Input value={email} onChange={(e) => setEmail(e.target.value)} />
+			</div>
+			<div>
+				<label>Password</label>
+				<Input
+					value={password}
+					onChange={(e) => setPassword(e.target.value)}
+					disabled
+				/>
+			</div>
+			<div>
+				<label>Age</label>
+				<Input value={age} onChange={(e) => setAge(e.target.value)} />
+			</div>
+			<div>
+				<label>Gender</label>
+				<Input value={gender} onChange={(e) => setGender(e.target.value)} />
+			</div>
+			<div>
+				<label>Address</label>
+				<Input value={address} onChange={(e) => setAddress(e.target.value)} />
+			</div>
+			<div>
+				<label>Role</label>
+				<Input value={role} onChange={(e) => setRole(e.target.value)} />
+			</div>
+		</Modal>
+	);
 };
 
 export default UpdateUserModal;
--- chapter-3-react-basic/react-vite-starter-master/src/components/users/users.table.tsx
+++ chapter-3-react-basic/react-vite-starter-master/src/components/users/users.table.tsx
@@ -7,201 +7,201 @@ import UpdateUserModal from "./update.user.modal";
 // import "../../styles/users.scss";
 
 export interface IUsers {
-  _id: string;
-  name: string;
-  email: string;
-  password: string;
-  age: string;
-  gender: string;
-  role: string;
-  address: string;
+     _id: string;
+     name: string;
+     email: string;
+     password: string;
+     age: string;
+     gender: string;
+     role: string;
+     address: string;
 }
 
 const UsersTable = () => {
-  const [listUsers, setListUsers] = useState([]);
-  const [accessToken, setAccessToken] = useState("");
-  const [isCreateModalOpen, setIsCreateModalOpen] = useState(false);
-  const [isUpdateModalOpen, setIsUpdateModalOpen] = useState(false);
-  const [dataUpdate, setDataUpdate] = useState<null | IUsers>(null);
+     const [listUsers, setListUsers] = useState([]);
+     const [accessToken, setAccessToken] = useState("");
+     const [isCreateModalOpen, setIsCreateModalOpen] = useState(false);
+     const [isUpdateModalOpen, setIsUpdateModalOpen] = useState(false);
+     const [dataUpdate, setDataUpdate] = useState<null | IUsers>(null);
 
-  const getData = async () => {
-    const res = await fetch("http://localhost:8000/api/v1/auth/login", {
-      method: "POST",
-      headers: {
-        "Content-Type": "application/json",
-      },
-      body: JSON.stringify({
-        username: "admin@gmail.com",
-        password: "123456",
-      }),
-    });
-    const data = await res.json();
-    const res1 = await fetch("http://localhost:8000/api/v1/users/all", {
-      method: "GET",
-      headers: {
-        "Content-Type": "application/json",
-        Authorization: `Bearer ${data.data.access_token}`,
-      },
-    });
+     const getData = async () => {
+          const res = await fetch("http://localhost:8000/api/v1/auth/login", {
+               method: "POST",
+               headers: {
+                    "Content-Type": "application/json",
+               },
+               body: JSON.stringify({
+                    username: "admin@gmail.com",
+                    password: "123456",
+               }),
+          });
+          const data = await res.json();
+          const res1 = await fetch("http://localhost:8000/api/v1/users/all", {
+               method: "GET",
+               headers: {
+                    "Content-Type": "application/json",
+                    Authorization: `Bearer ${data.data.access_token}`,
+               },
+          });
 
-    const users = await res1.json();
+          const users = await res1.json();
 
-    if (!users.data) {
-      notification.error({
-        message: JSON.stringify(users.message),
-      });
-    }
-    setListUsers(users.data.result);
-    setAccessToken(data.data.access_token);
-  };
+          if (!users.data) {
+               notification.error({
+                    message: JSON.stringify(users.message),
+               });
+          }
+          setListUsers(users.data.result);
+          setAccessToken(data.data.access_token);
+     };
 
-  useEffect(() => {
-    getData();
-  }, []);
+     useEffect(() => {
+          getData();
+     }, []);
 
-  const handleUpdateUser = (user: any) => {
-    setDataUpdate(user);
-    setIsUpdateModalOpen(true);
-  };
+     const handleUpdateUser = (user: any) => {
+          setDataUpdate(user);
+          setIsUpdateModalOpen(true);
+     };
 
-  const confirmDeleteUser = async (id: any) => {
-    const deleteUserId = await fetch(
-      `http://localhost:8000/api/v1/users/${id}`,
-      {
-        method: "DELETE",
-        headers: {
-          "Content-Type": "application/json",
-          Authorization: `Bearer ${accessToken}`,
-        },
-      }
-    );
+     const confirmDeleteUser = async (id: any) => {
+          const deleteUserId = await fetch(
+               `http://localhost:8000/api/v1/users/${id}`,
+               {
+                    method: "DELETE",
+                    headers: {
+                         "Content-Type": "application/json",
+                         Authorization: `Bearer ${accessToken}`,
+                    },
+               }
+          );
 
-    const deleteUser = await deleteUserId.json();
-    if (deleteUser.data) {
-      await getData();
-      message.success("Click on Yes");
-    }
-  };
+          const deleteUser = await deleteUserId.json();
+          if (deleteUser.data) {
+               await getData();
+               message.success("Click on Yes");
+          }
+     };
 
-  const columns: ColumnsType<IUsers> = [
-    {
-      title: "Name",
-      dataIndex: "name",
-      render: (value, record) => {
-        return <a href="/">{record.name}</a>;
-      },
-    },
-    {
-      title: "Email",
-      dataIndex: "email",
-    },
-    {
-      title: "Role",
-      dataIndex: "role",
-    },
-    {
-      title: "Action",
-      render: (value, record) => {
-        return (
-          <div>
-            <button onClick={() => handleUpdateUser(record)} className="mr-3">
-              Edit
-            </button>
-            <Popconfirm
-              title="Delete the user"
-              description={`Are you sure to delete ${record.name}?`}
-              onConfirm={() => confirmDeleteUser(record._id)}
-              okText="Yes"
-              cancelText="No"
-            >
-              <Button danger>Delete</Button>
-            </Popconfirm>
-          </div>
-        );
-      },
-    },
-  ];
+     const columns: ColumnsType<IUsers> = [
+          {
+               title: "Name",
+               dataIndex: "name",
+               render: (value, record) => {
+                    return <a href="/">{record.name}</a>;
+               },
+          },
+          {
+               title: "Email",
+               dataIndex: "email",
+          },
+          {
+               title: "Role",
+               dataIndex: "role",
+          },
+          {
+               title: "Action",
+               render: (value, record) => {
+                    return (
+                         <div>
+                              <button onClick={() => handleUpdateUser(record)} className="mr-3">
+                                   Edit
+                              </button>
+                              <Popconfirm
+                                   title="Delete the user"
+                                   description={`Are you sure to delete ${record.name}?`}
+                                   onConfirm={() => confirmDeleteUser(record._id)}
+                                   okText="Yes"
+                                   cancelText="No"
+                              >
+                                   <Button danger>Delete</Button>
+                              </Popconfirm>
+                         </div>
+                    );
+               },
+          },
+     ];
 
-  const addNewUser = async (data: any) => {
-    const newUser = await fetch("http://localhost:8000/api/v1/users", {
-      method: "POST",
-      headers: {
-        "Content-Type": "application/json",
-        Authorization: `Bearer ${accessToken}`,
-      },
-      body: JSON.stringify({ ...data }),
-    });
+     const addNewUser = async (data: any) => {
+          const newUser = await fetch("http://localhost:8000/api/v1/users", {
+               method: "POST",
+               headers: {
+                    "Content-Type": "application/json",
+                    Authorization: `Bearer ${accessToken}`,
+               },
+               body: JSON.stringify({ ...data }),
+          });
 
-    const isNewUser = await newUser.json();
+          const isNewUser = await newUser.json();
 
-    if (isNewUser.data) {
-      getData();
-      notification.success({
-        message: JSON.stringify(isNewUser.message),
-      });
-      setIsCreateModalOpen(false);
-    } else {
-      notification.error({
-        message: "Có lỗi xảy ra",
-        description: JSON.stringify(isNewUser.message),
-      });
-    }
-  };
+          if (isNewUser.data) {
+               getData();
+               notification.success({
+                    message: JSON.stringify(isNewUser.message),
+               });
+               setIsCreateModalOpen(false);
+          } else {
+               notification.error({
+                    message: "Có lỗi xảy ra",
+                    description: JSON.stringify(isNewUser.message),
+               });
+          }
+     };
 
-  const updateUser = async (data: any) => {
-    const updateUser = await fetch("http://localhost:8000/api/v1/users", {
-      method: "PATCH",
-      headers: {
-        "Content-Type": "application/json",
-        Authorization: `Bearer ${accessToken}`,
-      },
-      body: JSON.stringify({ ...data }),
-    });
+     const updateUser = async (data: any) => {
+          const updateUser = await fetch("http://localhost:8000/api/v1/users", {
+               method: "PATCH",
+               headers: {
+                    "Content-Type": "application/json",
+                    Authorization: `Bearer ${accessToken}`,
+               },
+               body: JSON.stringify({ ...data }),
+          });
 
-    const newUpdate = await updateUser.json();
-    if (newUpdate.data) {
-      getData();
-      notification.success({
-        message: JSON.stringify(newUpdate.message),
-      });
-    }
-  };
+          const newUpdate = await updateUser.json();
+          if (newUpdate.data) {
+               getData();
+               notification.success({
+                    message: JSON.stringify(newUpdate.message),
+               });
+          }
+     };
 
-  return (
-    <>
-      <div className="container mt-3">
-        <div className="pb-3 d-flex justify-content-between">
-          <h2 className="text-center">Table Users</h2>
-          <Button
-            type="primary"
-            onClick={() => {
-              setIsCreateModalOpen(true);
-            }}
-            icon={<PlusCircleOutlined />}
-          >
-            Open Modal
-          </Button>
-        </div>
+     return (
+          <>
+               <div className="container mt-3">
+                    <div className="pb-3 d-flex justify-content-between">
+                         <h2 className="text-center">Table Users</h2>
+                         <Button
+                              type="primary"
+                              onClick={() => {
+                                   setIsCreateModalOpen(true);
+                              }}
+                              icon={<PlusCircleOutlined />}
+                         >
+                              Open Modal
+                         </Button>
+                    </div>
 
-        <Table columns={columns} dataSource={listUsers} rowKey={"_id"} />
+                    <Table columns={columns} dataSource={listUsers} rowKey={"_id"} />
 
-        <CreateUserModal
-          addNewUser={addNewUser}
-          isCreateModalOpen={isCreateModalOpen}
-          setIsCreateModalOpen={setIsCreateModalOpen}
-        />
+                    <CreateUserModal
+                         addNewUser={addNewUser}
+                         isCreateModalOpen={isCreateModalOpen}
+                         setIsCreateModalOpen={setIsCreateModalOpen}
+                    />
 
-        <UpdateUserModal
-          addNewUser={addNewUser}
-          isUpdateModalOpen={isUpdateModalOpen}
-          setIsUpdateModalOpen={setIsUpdateModalOpen}
-          dataUpdate={dataUpdate}
-          setDataUpdate={setDataUpdate}
-          updateUser={updateUser}
-        />
-      </div>
-    </>
-  );
+                    <UpdateUserModal
+                         addNewUser={addNewUser}
+                         isUpdateModalOpen={isUpdateModalOpen}
+                         setIsUpdateModalOpen={setIsUpdateModalOpen}
+                         dataUpdate={dataUpdate}
+                         setDataUpdate={setDataUpdate}
+                         updateUser={updateUser}
+                    />
+               </div>
+          </>
+     );
 };
 
 export default UsersTable;
