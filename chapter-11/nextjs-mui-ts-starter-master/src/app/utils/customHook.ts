f9a402f15c3f0d93cdc74a890f8f8ca8f6323922
tringuyen1 <46176254+tringuyen1@users.noreply.github.com>
2023-10-24 23:40:13 +0700

    update wave track

--- chapter-11/nextjs-mui-ts-starter-master/src/app/utils/customHook.ts
+++ chapter-11/nextjs-mui-ts-starter-master/src/app/utils/customHook.ts
@@ -1,26 +1,51 @@
-import { useState, useEffect } from 'react';
-
+import { useState, useEffect, RefObject } from 'react';
+import WaveSurfer from "waveSurfer.js";
+import { WaveSurferOptions } from "waveSurfer.js";
 
 export const useHasMounted = () => {
-    const [hasMounted, setHasMounted] = useState<boolean>(false);
-    useEffect(() => {
-        setHasMounted(true);
-    }, []);
+	const [hasMounted, setHasMounted] = useState<boolean>(false);
+	useEffect(() => {
+		setHasMounted(true);
+	}, []);
 
-    return hasMounted;
+	return hasMounted;
 }
 
 export const useScript = (url: string) => {
-    useEffect(() => {
-        const script = document.createElement('script');
+	useEffect(() => {
+		const script = document.createElement('script');
 
-        script.src = url;
-        script.async = true;
+		script.src = url;
+		script.async = true;
 
-        document.body.appendChild(script);
+		document.body.appendChild(script);
 
-        return () => {
-            document.body.removeChild(script);
-        }
-    }, [url]);
+		return () => {
+			document.body.removeChild(script);
+		}
+	}, [url]);
+};
+
+export const useWaveSurfer = (
+	containerRef: RefObject<HTMLDivElement>,
+	options: Omit<WaveSurferOptions, "container">
+) => {
+	const [waveSurfer, setWaveSurfer] = useState<WaveSurfer | null>(null);
+
+	useEffect(() => {
+		if (!containerRef.current) return;
+
+		const ws = WaveSurfer.create({
+			...options,
+			container: containerRef.current,
+		});
+
+		setWaveSurfer(ws);
+
+		return () => {
+			ws.destroy();
+		};
+	}, [options, containerRef]);
+
+	return waveSurfer;
 };
--- chapter-11/nextjs-mui-ts-starter-master/src/components/track/wave.track.tsx
+++ chapter-11/nextjs-mui-ts-starter-master/src/components/track/wave.track.tsx
@@ -1,33 +1,91 @@
 "use Client";
 
-import { useEffect, useRef } from "react";
-import WaveSurfer from "wavesurfer.js";
+import { useRef, useState, useEffect, useCallback, useMemo } from "react";
 import { useSearchParams } from "next/navigation";
+import { useWaveSurfer } from "@/app/utils/customHook";
+import { WaveSurferOptions } from "waveSurfer.js";
 
-const WaveTrack = () => {
+const WaveTrack = (props: any) => {
+  const [isPlaying, setIsPlaying] = useState(false);
   const containerRef = useRef<HTMLDivElement>(null);
   const searchParams = useSearchParams();
-
   const fileName = searchParams.get("audio");
 
-  useEffect(() => {
-    if (containerRef.current) {
-      const waveSurfer = WaveSurfer.create({
-        container: containerRef.current,
-        waveColor: "rgb(200, 0, 200)",
-        progressColor: "rgb(100, 0, 100)",
-        url: `/api?audio=${fileName}`,
-      });
+  const optionsMemo = useMemo((): Omit<WaveSurferOptions, "container"> => {
+    const canvas = document.createElement("canvas");
+    const ctx = canvas.getContext("2d")!;
 
-      waveSurfer.on("click", () => {
-        waveSurfer.play();
-      });
+    const gradient = ctx.createLinearGradient(0, 0, 0, canvas.height * 1.35);
+    gradient.addColorStop(0, "#656666"); // Top color
+    gradient.addColorStop((canvas.height * 0.7) / canvas.height, "#656666"); // Top color
+    gradient.addColorStop((canvas.height * 0.7 + 1) / canvas.height, "#ffffff"); // White line
+    gradient.addColorStop((canvas.height * 0.7 + 2) / canvas.height, "#ffffff"); // White line
+    gradient.addColorStop((canvas.height * 0.7 + 3) / canvas.height, "#B1B1B1"); // Bottom color
+    gradient.addColorStop(1, "#B1B1B1"); // Bottom color
+
+    const progressGradient = ctx.createLinearGradient(
+      0,
+      0,
+      0,
+      canvas.height * 1.35
+    );
+    progressGradient.addColorStop(0, "#EE772F"); // Top color
+    progressGradient.addColorStop(
+      (canvas.height * 0.7) / canvas.height,
+      "#EB4926"
+    ); // Top color
+    progressGradient.addColorStop(
+      (canvas.height * 0.7 + 1) / canvas.height,
+      "#ffffff"
+    ); // White line
+    progressGradient.addColorStop(
+      (canvas.height * 0.7 + 2) / canvas.height,
+      "#ffffff"
+    ); // White line
+    progressGradient.addColorStop(
+      (canvas.height * 0.7 + 3) / canvas.height,
+      "#F6B094"
+    ); // Bottom color
+    progressGradient.addColorStop(1, "#F6B094"); // Bottom color
+
+    return {
+      progressColor: progressGradient,
+      barWidth: 2,
+      waveColor: gradient,
+      url: `/api?audio=${fileName}`,
+    };
+  }, []); // lưu lại giá trị value chỉ render 1 lần. biến số không thay đổi cho dù render bao nhiêu lần.
+
+  const waveSurfer = useWaveSurfer(containerRef, optionsMemo);
+
+  useEffect(() => {
+    if (!waveSurfer) return;
+
+    setIsPlaying(false);
+
+    const subscriptions = [
+      waveSurfer.on("play", () => setIsPlaying(true)),
+      waveSurfer.on("pause", () => setIsPlaying(false)),
+    ];
+
+    return () => {
+      subscriptions.forEach((unsub) => unsub());
+    };
+  }, [waveSurfer]);
+
+  const onPlayClick = useCallback(() => {
+    if (waveSurfer) {
+      waveSurfer.isPlaying() ? waveSurfer.pause() : waveSurfer.play();
+      setIsPlaying(waveSurfer.isPlaying());
     }
-  }, []);
+  }, [waveSurfer]); // lưu lại function
 
   return (
     <>
       <div ref={containerRef}>WaveTrack page</div>
+      <button onClick={() => onPlayClick()}>
+        {isPlaying ? "Pause" : "Play"}
+      </button>
     </>
   );
 };
