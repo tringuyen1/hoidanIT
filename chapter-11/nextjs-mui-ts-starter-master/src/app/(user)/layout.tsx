a577929fbf3f257600387a48a97a0927366171e2
tringuyen1 <46176254+tringuyen1@users.noreply.github.com>
2023-11-02 14:45:08 +0700

    update seo

--- chapter-11/nextjs-mui-ts-starter-master/src/app/(user)/layout.tsx
+++ chapter-11/nextjs-mui-ts-starter-master/src/app/(user)/layout.tsx
@@ -3,6 +3,9 @@ import AppHeader from "../../components/header/app.header";
 import AppFooter from "../../components/footer/app.footer";
 
 import type { Metadata } from 'next'
+import Script from 'next/script'
+import { idJsonObject } from "../constant/contstant";
+
 
 
 export const metadata: Metadata = {
@@ -18,6 +21,8 @@ export default function RootLayout({ children }: { children: React.ReactNode; })
                {children}
                <div style={{ marginTop: "100px" }}></div>
                <AppFooter />
+               <Script type="application/ld+json"
+                    dangerouslySetInnerHTML={{ __html: JSON.stringify(idJsonObject) }} />
           </>
      );
 }
