bea6bf4c7d963dbcde9f1ee30be4d23f770703e4
tringuyen1 <46176254+tringuyen1@users.noreply.github.com>
2023-11-04 17:08:38 +0700

    update middleware

--- chapter-11/nextjs-mui-ts-starter-master/src/app/api/auth/[...nextauth]/route.ts
+++ chapter-11/nextjs-mui-ts-starter-master/src/app/api/auth/[...nextauth]/route.ts
@@ -9,7 +9,7 @@ import CredentialsProvider from "next-auth/providers/credentials";
 import { JWT } from "next-auth/jwt";
 
 export const authOptions: AuthOptions = {
-    secret: process.env.NO_SECRET,
+    secret: process.env.NEXTAUTH_SECRET,
     // Configure one or more authentication providers
     providers: [
         CredentialsProvider({
--- chapter-11/nextjs-mui-ts-starter-master/src/app/not-found.tsx
+++ chapter-11/nextjs-mui-ts-starter-master/src/app/not-found.tsx
@@ -1,11 +1,29 @@
-import Link from 'next/link'
+
+
+import './error.scss';
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 export default function NotFound() {
      return (
           <div>
-               <h2>Not Found</h2>
-               <p>Could not find requested resource</p>
-               <Link href="/">Return Home</Link>
-          </div>
+               <link href="https://maxcdn.bootstrapcdn.com/font-awesome/4.3.0/css/font-awesome.min.css" rel="stylesheet" />
+               <div className="page-404">
+                    <div className="outer">
+                         <div className="middle">
+                              <div className="inner">
+
+                                   <div className="inner-circle"><i className="fa fa-home"></i><span>500</span></div>
+                                   <span className="inner-status">Oops! You're lost</span>
+                                   <span className="inner-detail">
+                                        <p>We can not find the page you're looking for.</p>
+                                        <a href="/" className="btn btn-info mtl"><i className="fa fa-home"></i>&nbsp;
+                                             Return home
+                                        </a>
+                                   </span>
+                              </div>
+                         </div>
+                    </div>
+               </div>
+          </div >
      )
 }
