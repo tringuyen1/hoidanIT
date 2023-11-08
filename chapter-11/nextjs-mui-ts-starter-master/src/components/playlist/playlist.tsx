d5b3e64788cd7261cc20e33b18f32b5e3656a483
tringuyen1 <46176254+tringuyen1@users.noreply.github.com>
2023-11-08 18:28:45 +0700

    update playlist

--- chapter-11/nextjs-mui-ts-starter-master/src/components/playlist/playlist.tsx
+++ chapter-11/nextjs-mui-ts-starter-master/src/components/playlist/playlist.tsx
@@ -10,6 +10,10 @@ import Button from '@mui/material/Button';
 import AddIcon from '@mui/icons-material/Add';
 import { useState } from 'react';
 import Modal from './modal';
+import PlayArrowIcon from '@mui/icons-material/PlayArrow';
+import PauseIcon from '@mui/icons-material/Pause';
+import IconButton from '@mui/material/IconButton';
+import { useTrackContext } from '@/lib/track.wrapper';
 
 interface IProps {
      playlist: any;
@@ -20,6 +24,8 @@ const PlayListTag = (props: IProps) => {
      const { playlist, tracks } = props;
      const [modal, setModal] = useState("")
      const [open, setOpen] = useState(false);
+     const [expanded, setExpanded] = useState<string | false>(false);
+     const { currentTrack, setCurrentTrack } = useTrackContext() as ITrackContext;
 
      const handleClickOpenPlaylist = () => {
           setOpen(true);
@@ -28,6 +34,15 @@ const PlayListTag = (props: IProps) => {
      const handleClickOpenTracks = () => {
           setOpen(true);
      };
+
+     const handleChangeAccordion = (panel: string) => (event: React.SyntheticEvent, isExpanded: boolean) => {
+          setExpanded(isExpanded ? panel : false);
+     };
+
+     const handlePlayPause = (play: boolean, data: any) => {
+          console.log("check >>>", data)
+          setCurrentTrack({ ...data, isPlaying: play });
+     }
 
      return (
           <div className='playlist-wrapper' style={{ marginTop: "30px" }}>
@@ -62,20 +77,29 @@ const PlayListTag = (props: IProps) => {
                <Container>
                     {
                          playlist.map((item: any) => (
-                              <Accordion key={item._id} sx={{ mt: "30px" }}>
+                              <Accordion expanded={expanded === item._id} onChange={handleChangeAccordion(item._id)} key={item._id} sx={{ mt: "30px" }}>
                                    <AccordionSummary
                                         expandIcon={<ExpandMoreIcon />}
                                         aria-controls="panel1a-content"
                                         id="panel1a-header"
                                    >
-                                        <Typography>{item.title}</Typography>
+                                        <Typography sx={{ textTransform: "capitalize" }}>{item.title}</Typography>
                                    </AccordionSummary>
                                    {
                                         item.tracks.length > 0 ? item.tracks.map((track: any) => (
                                              <AccordionDetails>
-                                                  <Typography>
-                                                       {track.title}
-                                                  </Typography>
+                                                  <div style={{ display: "flex" }}>
+                                                       <Typography>
+                                                            {track.title}
+                                                       </Typography>
+                                                       {(track._id === currentTrack._id && currentTrack.isPlaying === true) && (
+                                                            <PauseIcon onClick={() => handlePlayPause(false, track)} sx={{ ml: "auto" }} />
+                                                       )}
+                                                       {(track._id !== currentTrack._id || track._id === currentTrack._id && currentTrack.isPlaying === false) && (
+                                                            <PlayArrowIcon onClick={() => handlePlayPause(true, track)} sx={{ ml: "auto" }} />
+                                                       )}
+                                                  </div>
+
                                              </AccordionDetails>
                                         )) : (
                                              (
